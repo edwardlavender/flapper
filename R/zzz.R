6e87862d97b6b34943b1ddbf0ec783a23c793437
edwardlavender <el72@st-andrews.ac.uk>
2023-09-01 15:41:26 +0200

    Use utils::packageVersion()

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("This is {flapper} v.", packageVersion("flapper"), ". For an overview, see ?flapper. For support, contact edward.lavender@eawag.ch."))
+  packageStartupMessage(paste0("This is {flapper} v.", utils::packageVersion("flapper"), ". For an overview, see ?flapper. For support, contact edward.lavender@eawag.ch."))
 }
